36e9c53e3df211a977840e737ade5a5e7141b6bd
Diego Altamirano <diego@bonomedico.es>
2017-01-13 10:45:48 +0100

    Init

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php
+$bodyclass='notfound';
+require_once('includes/header.php'); ?>
+<h1>Page Not Found</h1>
+<p>Sorry, but the page you were trying to view does not exist.</p>
+<?php
+require_once('includes/header.php'); ?>
---
+++ private/gulpfile.js
@@ -0,0 +1,72 @@
+
+// Include gulp
+var gulp = require('gulp');
+
+// Include plugins
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var bower = require('gulp-bower');
+var mainBowerFiles = require('main-bower-files');
+var less = require('gulp-less');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+
+
+// Config
+var root='../';
+var config = {
+   lessPath: './resources/less/',
+  jsPath: './resources/scripts/',
+   bowerDir: './bower_components/' ,
+  publicCssPath: root+'css',
+  publicJsPath: root+'js',
+  publicFontsPath: root+'fonts'
+};
+
+// Task 'bower'
+gulp.task('bower', function() { 
+  return bower()
+     .pipe(gulp.dest(config.bowerDir)) 
+});
+
+// Task 'js'
+gulp.task('js', function() {
+  var files = mainBowerFiles('**/*.js').concat([config.jsPath+'*']);
+  gulp.src(files)
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(config.publicJsPath));  
+});
+
+// Task 'css'
+gulp.task('css', function() { 
+  var files =mainBowerFiles('**/*.less').concat([config.lessPath+'*.less']);
+  
+  // Preparo los css propios del proyecto:
+  gulp.src(files)
+    .pipe(less().on('error', function (err) {
+      console.log(err);
+    }))
+    .pipe(concat('main.css'))
+    .pipe(cssmin().on('error', function(err) {
+      console.log(err);
+    }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(config.publicCssPath));
+});
+
+// Task 'fonts'
+gulp.task('fonts', function() { 
+  return gulp.src(config.bowerDir + 'font-awesome/fonts/**.*') 
+    .pipe(gulp.dest(config.publicFontsPath)); 
+});
+
+// Default task
+gulp.task('default', ['bower', 'js', 'css', 'fonts']);
+
+// Rerun a task when a file changes
+ gulp.task('watch', function() {
+   gulp.watch([config.lessPath + '*.less'], ['css']); 
+  gulp.watch(config.jsPath + '*.js', ['js']); 
+});
